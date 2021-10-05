e2996f2461f8d563b13f0f48d4a4001727061e7c
Cyrus Ng <tun60613@temple.edu>
2021-10-05 00:56:51 -0400

    Commit 4

--- app/src/main/java/edu/temple/selectionactivity/DisplayActivity.kt
+++ app/src/main/java/edu/temple/selectionactivity/DisplayActivity.kt
@@ -1,11 +1,27 @@
 package edu.temple.selectionactivity
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
 
 class DisplayActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_display)
+
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val image = intent.getIntExtra(EXTRA_IMAGE,0)
+
+
+        val textView = findViewById<TextView>(R.id.actTextView).apply {
+            text = message
+        }
+
+        val iView = findViewById<ImageView>(R.id.actImageView)
+        iView.setImageResource(image)
     }
+
 }
--- app/src/main/java/edu/temple/selectionactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/selectionactivity/MainActivity.kt
@@ -1,11 +1,16 @@
 package edu.temple.selectionactivity
 
+
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.view.View
+
 
 val foodArray: Array<Image> = arrayOf(Image("sandwhich",R.drawable.chicken_sandwhich),
     Image("tofu",R.drawable.tofu_stew),Image("Fried Chicken",R.drawable.fried_chicken)
@@ -13,23 +18,29 @@ val foodArray: Array<Image> = arrayOf(Image("sandwhich",R.drawable.chicken_sandw
     ,Image("Onigiri",R.drawable.onigiri),Image("Pork Belly",R.drawable.pork_belly)
     ,Image("Pork Bun",R.drawable.pork_bun),Image("Ramen",R.drawable.ramen)
     ,Image("Steak",R.drawable.steak),Image("Sushi",R.drawable.sushi))
+
+const val EXTRA_MESSAGE = "edu.temple.selectionactivity.MESSAGE"
+const val EXTRA_IMAGE = "edu.temple.selectionactivity.IMAGE"
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        val imageView = findViewById<ImageView>(R.id.imageView)
-        val textView = findViewById<TextView>(R.id.textView)
         var adapter = ImageAdapter(foodArray)
         recyclerView.adapter=adapter
         recyclerView.layoutManager= GridLayoutManager(this, 3)
+
         adapter.setOnItemClickListener(object : ImageAdapter.onItemClickListener{
             override fun onItemClick(position: Int) {
-                imageView.setImageResource(foodArray[position].resourceId)
-                textView.setText(foodArray[position].description)
+                val message = foodArray[position].description
+                val image = foodArray[position].resourceId
+                val intent = Intent(this@MainActivity,DisplayActivity::class.java).apply{
+                    putExtra(EXTRA_MESSAGE,message)
+                    putExtra(EXTRA_IMAGE,image)
+                }
+                startActivity(intent)
             }
-
         })
-
     }
 }
